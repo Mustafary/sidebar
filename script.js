2a8738f3880d1417fd8a46eee2818f18978162b9
Raelsouza10 <is.raelsouza1014@gmail.com>
2023-03-23 18:30:33 -0300

    add sidebar

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenu (){
+  const menuMobile = document.getElementById("menu-mobile")
+
+  if(menuMobile.className === "menu-mobile-active"){
+    menuMobile.className = "menu-mobile"
+  }
+
+  else{
+    menuMobile.className = "menu-mobile-active"
+
+  }
+}
